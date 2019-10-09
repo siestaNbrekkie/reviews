c8176c181a5f067cefe4fa8e0dde42c1afc53bce
Derek Parson <parson.derek@gmail.com>
2019-10-08 20:25:35 -0700

    Initial commit, folders added, package.json added, webpack added

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var webpack = require('webpack');
+var path = require('path');
+
+
+var config = {
+  entry: "./client/index.jsx",
+    output: {
+      filename: "main.js",
+      path: path.resolve(__dirname, "public")
+    },
+    resolve: {
+      extensions: [".jsx", ".js", ".json"]
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.(js | jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env", "@babel/preset-react"]
+            }
+          }
+        }
+      ]
+    },
+    mode: "development"
+  };
+
